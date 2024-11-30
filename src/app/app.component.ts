622ec8995a282b0faa64b51828168660aced3906
Machado21 <grmachado12@hotmail.com>
2024-11-29 21:19:21 -0400

    feat: The layout changes from sidebar to navbar depending on the size of the screen.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+
 import { NavBarComponent } from "./components/shared/nav-bar/nav-bar.component";
 import { FooterComponent } from "./components/shared/footer/footer.component";
 import { SideBarComponent } from "./components/shared/side-bar/side-bar.component";
@@ -11,6 +13,16 @@ import { SideBarComponent } from "./components/shared/side-bar/side-bar.componen
   standalone: true,
   imports: [RouterOutlet, NavBarComponent, FooterComponent, SideBarComponent],
 })
-export class AppComponent {
-  title = "Auth0 Angular SDK Sample";
+export class AppComponent implements OnInit {
+  isSmallScreen: boolean;
+
+  constructor(private breackpointObserver: BreakpointObserver) {}
+  
+  ngOnInit() {
+    this.breackpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      this.isSmallScreen = result.matches;
+      console.log('isSmallScreen:', this.isSmallScreen);
+    });
+  }
 }
+

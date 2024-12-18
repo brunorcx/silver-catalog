f5bb29b4ec1fc635bf1d80c35b2efc40f445ea2c
Machado21 <grmachado12@hotmail.com>
2024-12-17 23:06:16 -0400

    style: (navbar) Navibar layout completed.
    Navibar and Sidebar layout completed
    
    Closes: #5

--- src/app/components/shared/nav-bar/nav-bar.component.ts
+++ src/app/components/shared/nav-bar/nav-bar.component.ts
@@ -19,6 +19,7 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class NavBarComponent implements OnInit, OnDestroy {
   isCollapsed = true;
+  userCollapsed = true;
   faUser = faUser;
   faPowerOff = faPowerOff;
   user: any;
